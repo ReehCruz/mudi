6fa8a68f5c7c54b178420244df9f68dae356fec1
REBECA <REBECACRUZ@ME.COM>
2020-12-29 20:02:58 -0300

    Validando

--- src/main/java/com/alura/mvc/mudi/dto/RequisicaoNovoPedido.java
+++ src/main/java/com/alura/mvc/mudi/dto/RequisicaoNovoPedido.java
@@ -1,12 +1,15 @@
 package com.alura.mvc.mudi.dto;
 
+import javax.validation.constraints.NotBlank;
+
 import com.alura.mvc.mudi.model.Pedido;
 
 public class RequisicaoNovoPedido {
 	
-	private String nomeProduto;
-	private String urlProduto;
-	private String urlImagem;
+	
+	@NotBlank private String nomeProduto;
+	@NotBlank private String urlProduto;
+	@NotBlank private String urlImagem;
 	private String descricao;
 	
 	public String getNomeProduto() {
